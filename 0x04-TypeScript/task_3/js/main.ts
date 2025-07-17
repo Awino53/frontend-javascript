ddc585161d9f166995f75cecbb1c372acf6f7bf2
Awino53 <estherojul53@gmail.com>
2025-07-17 21:29:03 +0300

    task4

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,89 +1,19 @@
-// 1. DirectorInterface
-interface DirectorInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workDirectorTasks(): string;
-}
+/// <reference path="./crud.d.ts" />
 
-// 2. TeacherInterface
-interface TeacherInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workTeacherTasks(): string;
-}
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
 
-// 3. Director class
-class Director implements DirectorInterface {
-  workFromHome(): string {
-    return 'Working from home';
-  }
-  getCoffeeBreak(): string {
-    return 'Getting a coffee break';
-  }
-  workDirectorTasks(): string {
-    return 'Getting to director tasks';
-  }
-}
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+};
 
-// 4. Teacher class
-class Teacher implements TeacherInterface {
-  workFromHome(): string {
-    return 'Cannot work from home';
-  }
-  getCoffeeBreak(): string {
-    return 'Cannot have a break';
-  }
-  workTeacherTasks(): string {
-    return 'Getting to work';
-  }
-}
+const newRowID: RowID = CRUD.insertRow(row);
 
-// 5. createEmployee function
-function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === 'number' && salary < 500) {
-    return new Teacher();
-  } else {
-    return new Director();
-  }
-}
+const updatedRow: RowElement = {
+  ...row,
+  age: 23,
+};
 
-function isDirector(employee: Director | Teacher): employee is Director {
-  return (employee as Director).workDirectorTasks !== undefined;
-}
-
-function executeWork(employee: Director | Teacher): string {
-  if (isDirector(employee)) {
-    return employee.workDirectorTasks();
-  } else {
-    return employee.workTeacherTasks();
-  }
-}
-
-
-
-// 6. Testing
-console.log(createEmployee(200));   // Teacher
-console.log(createEmployee(1000));  // Director
-console.log(createEmployee('$500')); // Director
-
-console.log(executeWork(createEmployee(200))); // Output: Getting to work
-console.log(executeWork(createEmployee(1000))); // Output: Getting to director tasks
-
-
-// Define string literal type
-type Subjects = 'Math' | 'History';
-
-// Function to teach based on subject
-function teachClass(todayClass: Subjects): string {
-  if (todayClass === 'Math') {
-    return 'Teaching Math';
-  } else {
-    return 'Teaching History';
-  }
-}
-
-// Test calls
-console.log(teachClass('Math'));     // Teaching Math
-console.log(teachClass('History'));  // Teaching History
-
-
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
